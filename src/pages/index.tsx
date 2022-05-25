33f677455d978f2a746ca8739d7533f81a3e8359
ysuzuki <ysuzuki@bysystems.jp>
2022-05-25 09:41:35 +0900

    Change style.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 
 function Loading() {
-  return <>(loading...)</>;
+  return <div style={{ color: "silver" }}>(loading...)</div>;
 }
 
 function NotHas() {
@@ -37,7 +37,11 @@ export default function Home(): React.ReactNode {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🤔</text></svg>"
         />
       </Head>
-      <main>{content}</main>
+      <main style={{ display: "flex", height: "calc(100vh - 8px - 8px)", fontSize: "84px", lineBreak: "anywhere", lineHeight: 1, fontFamily: "monospace" }}>
+        <div style={{ margin: "auto" }}>
+          {content}
+        </div>
+      </main>
     </>
   );
 }
